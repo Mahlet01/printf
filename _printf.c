0e0e33608fd0aaf5d9fde94d661278f14741e931
nahom1256 <theweeknd1256@gmail.com>
2023-10-16 12:02:01 -0700

    2nd commit

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Custom Printf function
+ * _printf - custom Printf function
  * @format: format.
  * Return: Printed chars.
  */
@@ -53,7 +53,7 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if the buffer exist
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
